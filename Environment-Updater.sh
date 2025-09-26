11e898e6399fc669bbbc9c4ad007121fa4868d4a
Eniti#8759 <106023124+Eniti-Codes@users.noreply.github.com>
2025-09-26 11:18:09 -0400

    Update Environment-Updater.sh

--- Environment-Updater.sh
+++ Environment-Updater.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Hotkey Engineer Plugins Environment Updater - Modify '$SERVICE_FILE' to include environment variables in ExecStart
+#
+# Copyright (C) 2025 Eniti-Codes
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 # --- Configuration ---
 SERVICE_NAME="hotkey-engineer.service"
 SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}"
